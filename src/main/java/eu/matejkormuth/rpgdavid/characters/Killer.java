7a60ac229d443ed82047b0c83b84a6c2ff46fb98
dobrakmato <dobrakmato@zoho.com>
2015-01-13 20:02:29 +0100

    Killer now has splash potions instead of not splash

--- src/main/java/eu/matejkormuth/rpgdavid/characters/Killer.java
+++ src/main/java/eu/matejkormuth/rpgdavid/characters/Killer.java
@@ -1,15 +1,17 @@
 package eu.matejkormuth.rpgdavid.characters;
 
 import org.bukkit.Color;
-import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionType;
 
 import eu.matejkormuth.rpgdavid.Character;
 import eu.matejkormuth.rpgdavid.Modifiers;
+import eu.matejkormuth.rpgdavid.bukkitfixes.WorkingPotion;
 import eu.matejkormuth.rpgdavid.inventoryutils.Armor;
 
 public class Killer extends Character {
 	public Killer() {
-		super("Vrah", null, new Modifiers(1, 1, 1, 1.5F, 1), Armor.createLether(Color.BLACK), new Potion(PotionType.INSTANT_DAMAGE, 1).toItemStack(4));
+		super("Vrah", null, new Modifiers(1, 1, 1, 1.5F, 1), Armor
+				.createLether(Color.BLACK), new WorkingPotion(
+				PotionType.INSTANT_DAMAGE, 1).splash().toItemStack(4));
 	}
 }
