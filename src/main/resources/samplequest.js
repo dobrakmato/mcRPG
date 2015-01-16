74307bb4372f1e49618fd4b265c656a40b1bd255
dobrakmato <dobrakmato@zoho.com>
2015-01-16 18:53:52 +0100

    sample js quest

---
+++ src/main/resources/samplequest.js
@@ -0,0 +1,27 @@
+/*
+ * Quest 01 - Introduction to magic.
+ * 
+ * Player has to go find old jerk Jack. Jack will give player magic stick. Player 
+ * then have to go to Alice on the other side of map. Alice will tell player 
+ * how to use magic stick (she will teach player first spell).
+ * 
+ * Author: dobrakmato.
+ */
+
+// Create quest object by extending JavascriptQuest class.
+var quest = new JavaAdapter(eu.matejkormuth.rpgdavid.quests.JavascriptQuest, {
+	// Name of the quest.
+	name : "Introduction to magic",
+	// Location of quest start.
+	locX : 0,
+	locY : 0,
+	locZ : 0,
+
+	// Called when quest is loaded.
+	prepeare : function() {
+		// Spawn NPCs and prepeare everything for quest.
+	}
+});
+
+// Optional: Explicitly add quest to game.
+manager.addQuest(quest);
