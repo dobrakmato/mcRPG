93300efef400c10ee14e052f927dbb32b9a3a02b
dobrakmato <dobrakmato@zoho.com>
2015-02-20 19:02:13 +0100

    starving: adds characters

---
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/Characters.java
@@ -0,0 +1,32 @@
+/*
+ *  Starving is a open source bukkit/spigot mmo game.
+ *  Copyright (C) 2014-2015 Matej Kormuth
+ *  This file is a part of Starving. <http://www.starving.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package eu.matejkormuth.rpgdavid.starving;
+
+import eu.matejkormuth.rpgdavid.starving.characters.Heavy;
+import eu.matejkormuth.rpgdavid.starving.characters.Scout;
+import eu.matejkormuth.rpgdavid.Character;
+
+public class Characters {
+    private Characters() {
+    }
+
+    public static final Character HEAVY = new Heavy();
+    public static final Character SCOUT = new Scout();
+}
