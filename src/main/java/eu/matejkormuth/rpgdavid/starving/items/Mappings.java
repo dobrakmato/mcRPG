aa7f116570d08c5b119ea65f28dad55216bc9555
dobrakmato <dobrakmato@zoho.com>
2015-02-21 22:25:53 +0100

    starving: adds generation of Mappings class from items.mapping file

---
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/items/Mappings.java
@@ -0,0 +1,77 @@
+package eu.matejkormuth.rpgdavid.starving.items;
+
+// This file is part of Starving 2.0 project.
+// This file was automatically generated.
+// Changes to this file wont be preserved!
+// Generated at: 21.2.2015 22:20
+// This file is licensed under GPL-3.
+// Starving: <http://github.com/dobrakmato/mcRPG>
+
+import org.bukkit.Material;
+
+public class Mappings {
+    private Mappings() {
+    }
+
+    // "Parachute" -> GOLD_HOE:0
+    public static final Material PARACHUTE = Material.GOLD_HOE;
+    // "Chemicals" -> POTION:0
+    public static final Material CHEMICALS = Material.POTION;
+    // "Transmitter" -> PRISMARINE_SHARD:0
+    public static final Material TRANSMITTER = Material.PRISMARINE_SHARD;
+    // "Patch" -> SLIME_BALL:0
+    public static final Material PATCH = Material.SLIME_BALL;
+    // "Projectile" -> SNOW_BALL:0
+    public static final Material PROJECTILE = Material.SNOW_BALL;
+    // "Bandage" -> PAPER:0
+    public static final Material BANDAGE = Material.PAPER;
+    // "Voltaic Battery" -> FERMENTED_SPIDER_EYE:0
+    public static final Material VOLTAIC_BATTERY = Material.FERMENTED_SPIDER_EYE;
+    // "Lantern" -> BLAZE_POWDER:0
+    public static final Material LANTERN = Material.BLAZE_POWDER;
+    // "Splint" -> PRISMARINE_CRYSTALS:0
+    public static final Material SPLINT = Material.PRISMARINE_CRYSTALS;
+    // "Coin" -> GOLD_NUGGET:0
+    public static final Material COIN = Material.GOLD_NUGGET;
+    // "Cap" -> LEATHER_HELMET:0
+    public static final Material CAP = Material.LEATHER_HELMET;
+    // "T-Shirt" -> LEATHER_CHESTPLATE:0
+    public static final Material T_SHIRT = Material.LEATHER_CHESTPLATE;
+    // "Jeans" -> LEATHER_LEGGINGS:0
+    public static final Material JEANS = Material.LEATHER_LEGGINGS;
+    // "Shoes" -> LEATHER_BOOTS:0
+    public static final Material SHOES = Material.LEATHER_BOOTS;
+    // "Hat" -> CHAINMAIL_HELMET:0
+    public static final Material HAT = Material.CHAINMAIL_HELMET;
+    // "Shirt" -> CHAINMAIL_CHESTPLATE:0
+    public static final Material SHIRT = Material.CHAINMAIL_CHESTPLATE;
+    // "Shorts" -> CHAINMAIL_LEGGINGS:0
+    public static final Material SHORTS = Material.CHAINMAIL_LEGGINGS;
+    // "Sandals" -> CHAINMAIL_BOOTS:0
+    public static final Material SANDALS = Material.CHAINMAIL_BOOTS;
+    // "RemVest" -> IRON_HELMET:0
+    public static final Material REMVEST = Material.IRON_HELMET;
+    // "Windbreaker" -> IRON_CHESTPLATE:0
+    public static final Material WINDBREAKER = Material.IRON_CHESTPLATE;
+    // "WinterPants" -> IRON_LEGGINGS:0
+    public static final Material WINTERPANTS = Material.IRON_LEGGINGS;
+    // "Boots" -> IRON_BOOTS:0
+    public static final Material BOOTS = Material.IRON_BOOTS;
+    // "CamoflageHelmet" -> GOLD_HELMET:0
+    public static final Material CAMOFLAGEHELMET = Material.GOLD_HELMET;
+    // "CamoflageThickShirt" -> GOLD_CHESTPLATE:0
+    public static final Material CAMOFLAGETHICKSHIRT = Material.GOLD_CHESTPLATE;
+    // "CamoflageThickPants" -> GOLD_LEGGINGS:0
+    public static final Material CAMOFLAGETHICKPANTS = Material.GOLD_LEGGINGS;
+    // "RubberShoes" -> GOLD_BOOTS:0
+    public static final Material RUBBERSHOES = Material.GOLD_BOOTS;
+    // "Shield" -> DIAMOND_HELMET:0
+    public static final Material SHIELD = Material.DIAMOND_HELMET;
+    // "BulletproofVest" -> DIAMOND_CHESTPLATE:0
+    public static final Material BULLETPROOFVEST = Material.DIAMOND_CHESTPLATE;
+    // "ThickPants" -> DIAMOND_LEGGINGS:0
+    public static final Material THICKPANTS = Material.DIAMOND_LEGGINGS;
+    // "ThickShoes" -> DIAMOND_BOOTS:0
+    public static final Material THICKSHOES = Material.DIAMOND_BOOTS;
+}
+
