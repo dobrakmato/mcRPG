7862c42552b89763592540d94f9dfbd54035fbed
dobrakmato <dobrakmato@zoho.com>
2015-02-22 11:50:40 +0100

    starving: disables mobdrops, adds tools

---
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/listeners/MobDropsListener.java
@@ -0,0 +1,32 @@
+/*
+ *  Starving is a open source bukkit/spigot mmo game.
+ *  Copyright (C) 2014-2015 Matej Kormuth
+ *  This file is a part of Starving. <http://www.starving.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package eu.matejkormuth.rpgdavid.starving.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+public class MobDropsListener implements Listener {
+    @EventHandler
+    private void onMobDeath(final EntityDeathEvent event) {
+        // Disable drops entirely
+        event.getDrops().clear();
+    }
+}
