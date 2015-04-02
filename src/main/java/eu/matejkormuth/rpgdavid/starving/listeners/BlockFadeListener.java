71db78a5150f3d6b623ba5a1d09812e4f806dade
Matej Kormuth <dobrakmato@gmail.com>
2015-04-02 11:07:32 +0200

    starving: adds parsistable options for BlockFadeListener

--- src/main/java/eu/matejkormuth/rpgdavid/starving/listeners/BlockFadeListener.java
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/listeners/BlockFadeListener.java
@@ -23,22 +23,72 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.block.LeavesDecayEvent;
+
+import eu.matejkormuth.rpgdavid.starving.persistence.Persist;
 
 public class BlockFadeListener implements Listener {
 
+    @Persist(key = "DISABLE_BLOCK_FADE")
+    private static boolean DISABLE_BLOCK_FADE = true;
+
+    @Persist(key = "DISABLE_BLOCK_FORM")
+    private static boolean DISABLE_BLOCK_FORM = true;
+
+    @Persist(key = "DISABLE_BLOCK_SPREAD")
+    private static boolean DISABLE_BLOCK_SPREAD = true;
+
+    @Persist(key = "DISABLE_BLOCK_FROMTO")
+    private static boolean DISABLE_BLOCK_FROMTO = true;
+
+    @Persist(key = "DISABLE_BLOCK_GROW")
+    private static boolean DISABLE_BLOCK_GROW = true;
+
+    @Persist(key = "DISABLE_LEAVES_DECAY")
+    private static boolean DISABLE_LEAVES_DECAY = true;
+
     @EventHandler
     private void onBlockFade(final BlockFadeEvent event) {
-        event.setCancelled(true);
+        if (DISABLE_BLOCK_FADE) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
     private void onBlockForm(final BlockFormEvent event) {
-        event.setCancelled(true);
+        if (DISABLE_BLOCK_FORM) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
     private void onBlockSpread(final BlockSpreadEvent event) {
-        event.setCancelled(true);
+        if (DISABLE_BLOCK_SPREAD) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    private void onBlockFromTo(final BlockFromToEvent event) {
+        if (DISABLE_BLOCK_FROMTO) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    private void onBlockGrow(final BlockGrowEvent event) {
+        if (DISABLE_BLOCK_GROW) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    private void onLeavesDecay(final LeavesDecayEvent event) {
+        if (DISABLE_LEAVES_DECAY) {
+            event.setCancelled(true);
+        }
     }
 }
