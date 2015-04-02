1b125fbc6c9ef505f5b6b5333fa0febb1515b24c
Matej Kormuth <dobrakmato@gmail.com>
2015-04-03 01:15:44 +0200

    starving: improved default zombies

--- src/main/java/eu/matejkormuth/rpgdavid/starving/listeners/ZombieListener.java
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/listeners/ZombieListener.java
@@ -19,15 +19,34 @@
  */
 package eu.matejkormuth.rpgdavid.starving.listeners;
 
+import net.minecraft.server.v1_8_R1.EntityZombie;
+import net.minecraft.server.v1_8_R1.GenericAttributes;
+
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftZombie;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
 import eu.matejkormuth.rpgdavid.starving.Starving;
+import eu.matejkormuth.rpgdavid.starving.persistence.Persist;
 
 public class ZombieListener implements Listener {
+
+    @Persist(key = "ZOMBIE_FOLLOW_RANGE")
+    private static float ZOMBIE_FOLLOW_RANGE = 32f;
+
+    @Persist(key = "ZOMBIE_MIN_SPEED")
+    private static float ZOMBIE_MIN_SPEED = 0.2899999988f;
+
+    @Persist(key = "ZOMBIE_MAX_SPEED")
+    private static float ZOMBIE_MAX_SPEED = 0.499999988f;
+
+    private static float ZOMBIE_SPEED_DIFF = ZOMBIE_MAX_SPEED
+            - ZOMBIE_MIN_SPEED;
+
     @EventHandler
     private void onChunkUnload(final ChunkUnloadEvent event) {
         for (Entity e : event.getChunk().getEntities()) {
@@ -36,6 +55,19 @@ public class ZombieListener implements Listener {
                     Starving.getInstance().getZombieManager().remove(e);
                 }
             }
+        }
+    }
+
+    @EventHandler
+    private void onZombieSpawn(final EntitySpawnEvent event) {
+        if (event.getEntityType() == EntityType.ZOMBIE) {
+            EntityZombie ez = ((CraftZombie) event.getEntity()).getHandle();
+            ez.getAttributeInstance(GenericAttributes.b).setValue(
+                    ZOMBIE_FOLLOW_RANGE); // follow
+            // range
+            ez.getAttributeInstance(GenericAttributes.d).setValue(
+                    ZOMBIE_MIN_SPEED + Math.random() * ZOMBIE_SPEED_DIFF); // movement
+            // speeds
         }
     }
 }
