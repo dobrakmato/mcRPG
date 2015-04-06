c79111b15bab16eac766b482e7b32261114095b7
Matej Kormuth <dobrakmato@gmail.com>
2015-04-06 15:52:59 +0200

    starving: adds explosive items placeholders

---
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/items/firearms/Revolver.java
@@ -0,0 +1,13 @@
+package eu.matejkormuth.rpgdavid.starving.items.firearms;
+
+import eu.matejkormuth.rpgdavid.starving.items.Mapping;
+import eu.matejkormuth.rpgdavid.starving.items.base.Firearm;
+
+public class Revolver extends Firearm {
+
+    public Revolver(Mapping mapping, String name) {
+        super(mapping, name);
+        // TODO Auto-generated constructor stub
+    }
+
+}
