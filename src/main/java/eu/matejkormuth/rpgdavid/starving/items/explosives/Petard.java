5aaab28260db60a6b99f7c81feda3afb6ea25994
Matej Kormuth <dobrakmato@gmail.com>
2015-04-18 23:23:02 +0200

    starving: implements petard

--- src/main/java/eu/matejkormuth/rpgdavid/starving/items/explosives/Petard.java
+++ src/main/java/eu/matejkormuth/rpgdavid/starving/items/explosives/Petard.java
@@ -19,11 +19,58 @@
  */
 package eu.matejkormuth.rpgdavid.starving.items.explosives;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+
+import com.darkblade12.particleeffect.ParticleEffect;
+
+import eu.matejkormuth.bukkit.Actions;
+import eu.matejkormuth.bukkit.ItemDrops;
+import eu.matejkormuth.bukkit.Items;
+import eu.matejkormuth.rpgdavid.starving.Time;
+import eu.matejkormuth.rpgdavid.starving.items.InteractResult;
 import eu.matejkormuth.rpgdavid.starving.items.Mappings;
 import eu.matejkormuth.rpgdavid.starving.items.base.Item;
+import eu.matejkormuth.rpgdavid.starving.tasks.RepeatingTask;
 
 public class Petard extends Item {
     public Petard() {
         super(Mappings.PETARD, "Petard");
+    }
+
+    @Override
+    public InteractResult onInteract(Player player, Action action,
+            Block clickedBlock, BlockFace clickedFace) {
+        if (Actions.isRightClick(action)) {
+            ItemStack is = Items.of(Mappings.PETARD.getMaterial());
+            org.bukkit.entity.Item i = ItemDrops.drop(player.getEyeLocation(),
+                    is, Time.ofSeconds(6).toTicks());
+            i.setVelocity(player.getEyeLocation().getDirection());
+
+            new RepeatingTask() {
+                private int ticks = 0;
+
+                @Override
+                public void run() {
+                    if (ticks >= 20 * 5) {
+                        this.cancel();
+                        this.explode();
+                    }
+                    ParticleEffect.FLAME.display(0.1f, 0.1f, 0.1f, 1, 5,
+                            i.getLocation(), Double.MAX_VALUE);
+                    ticks++;
+                }
+
+                private void explode() {
+                    i.getWorld().createExplosion(i.getLocation(), 0.001f);
+                    i.remove();
+                }
+            }.schedule(1L);
+        }
+
+        return InteractResult.useOne();
     }
 }
